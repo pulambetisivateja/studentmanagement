2ae645d7669b5332210c3b3dac9a9e97b3a1614a
sivatejap7 <sivatejap7@gmail.com>
2026-02-22 11:59:09 +0530

    feature(welcome and add API)

--- src/main/java/com/example/studentmanagement/controller/HelloController.java
+++ src/main/java/com/example/studentmanagement/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.studentmanagement.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,14 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello, this is my first REST API!";
+    }
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Spring Boot REST API!";
+    }
+    @GetMapping("/add")
+    public String add(@RequestParam int a, @RequestParam int b) {
+        int sum = a + b;
+        return "Sum is: " + sum;
     }
 }
