e08c0a69648d8159cfd7cea852e5ff04d3e23c6b
sivatejap7 <sivatejap7@gmail.com>
2026-02-25 06:56:18 +0530

    feat: add name REST API and UI to display name

--- src/main/java/com/example/studentmanagement/controller/HelloController.java
+++ src/main/java/com/example/studentmanagement/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
@@ -20,4 +23,13 @@ public class HelloController {
         int sum = a + b;
         return "Sum is: " + sum;
     }
+    @GetMapping("/api/name")
+    public Map<String, String> getName() {
+
+        Map<String, String> response = new HashMap<>();
+        response.put("name", "SUREKHA");
+
+        return response;
+    }
+
 }
